d43124e139a2fce8bfdb4d79a5cdadf4477ffb65
BillyS-PV <billsalme@gmail.com>
2023-05-22 22:54:40 -0500

    Subido

---
+++ src/shopping-list.ts
@@ -0,0 +1,11 @@
+let myList: [string, number][] = [
+    ["cereal", 3],
+    ["milk", 2],
+    ["bananas", 3],
+    ["juice", 2],
+    ["eggs", 12]
+  ];
+  console.log(myList[0]);
+  console.log(myList[4]);
+  console.log(myList[1]);
+  console.log(myList[2]);
